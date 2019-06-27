b9eee2899ba1f4833079b396f78cc56ee202120d
cruel32 <cshee8508@gmail.com>
2019-06-27 23:32:12 +0900

    cpp

---
+++ cpp/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    a = 10;
+    b = 20;
+    cout << "Hello, World!" << ", " << a << ", " << b << ", "<< endl;
+    return 0;
+}
