5212d00e7ba27cb24573988648e5210cc694461a
cruel-32 <cruel32@nate.com>
2019-07-01 20:48:15 +0900

    cpp bitwise 연습중

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -8,5 +8,12 @@ int main()
     a = 10;
     b = 20;
     cout << "Hello, World!" << ", " << a << ", " << b << ", "<< endl;
+
+    //bitwise 간단 문제. 제 3의 변수 선언 없이 a와 b의 값을 서로 바꿔라
+    a^=b;
+    b^=a;
+    a^=b;
+    cout << "어맛 값이 바뀌었어!" << ", " << a << ", " << b << ", "<< endl;
+
     return 0;
 }
