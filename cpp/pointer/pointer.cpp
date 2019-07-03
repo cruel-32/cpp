9c79fa2740e1400796e7d818467731ec0ce65469
cruel32 <cshee8508@gmail.com>
2019-07-04 00:39:01 +0900

    포인터 별거 없네

--- cpp/pointer/pointer.cpp
+++ cpp/pointer/pointer.cpp
@@ -16,5 +16,33 @@ int main()
     pB = &intA;
     cout << "*pB = " << *pB << "\n";
 
+
+
+    int a = 100;
+    int* iP = &a; //100
+    int* r = iP+2; //주소값 +8바이트
+
+    cout << "r = " << r << "\n";
+
+    short arr[5];
+    short* sP = &arr[3]; //arr[3]의 주소값
+    short* sQ = &arr[0]; //arr[0]의 주소값
+
+    int sr = sP - sQ;//원소 차이를 반환 = 3
+
+    cout << "sr = " << sr << "\n";
+
+
+    struct ABC {
+        int x;
+    } abc;
+
+    ABC* abcP = &abc;
+
+    //abc.x의 값을 30으로 바꾸는 방법
+    (*abcP).x = 30;
+
+    cout << "abc.x = " << abc.x << "\n";
+    
     return 0;
 }
